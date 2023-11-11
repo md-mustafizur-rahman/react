3ee2191ecb76d2271e1594565d036ca52c7e59aa
sabbirpegon <sabbirwalton343@gmail.com>
2023-11-11 23:22:54 +0600

    add

--- Basic Design/project-1/src/component/Header.jsx
+++ Basic Design/project-1/src/component/Header.jsx
@@ -1,6 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Header = () => {
+  const [searchQuery, setSearchQuery] = useState("");
+
+  const handleSearchChange = (e) => {
+    setSearchQuery(e.target.value);
+  };
+
+  const handleSearchSubmit = (e) => {
+    e.preventDefault();
+    // You can perform the search logic here, e.g., navigate to a search results page
+    console.log("Search submitted:", searchQuery);
+  };
+
   return (
     <>
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
@@ -21,65 +33,16 @@ const Header = () => {
           </button>
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-              <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="#">
-                  Home
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">
-                  Link
-                </a>
-              </li>
-              <li className="nav-item dropdown">
-                <a
-                  className="nav-link dropdown-toggle"
-                  href="#"
-                  id="navbarDropdown"
-                  role="button"
-                  data-bs-toggle="dropdown"
-                  aria-expanded="false"
-                >
-                  Dropdown
-                </a>
-                <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-                  <li>
-                    <a className="dropdown-item" href="#">
-                      Action
-                    </a>
-                  </li>
-                  <li>
-                    <a className="dropdown-item" href="#">
-                      Another action
-                    </a>
-                  </li>
-                  <li>
-                    <hr className="dropdown-divider" />
-                  </li>
-                  <li>
-                    <a className="dropdown-item" href="#">
-                      Something else here
-                    </a>
-                  </li>
-                </ul>
-              </li>
-              <li className="nav-item">
-                <a
-                  className="nav-link disabled"
-                  href="#"
-                  tabindex="-1"
-                  aria-disabled="true"
-                >
-                  Disabled
-                </a>
-              </li>
+              {/* ... your other navigation items ... */}
             </ul>
-            <form className="d-flex">
+            <form className="d-flex" onSubmit={handleSearchSubmit}>
               <input
                 className="form-control me-2"
                 type="search"
                 placeholder="Search"
                 aria-label="Search"
+                value={searchQuery}
+                onChange={handleSearchChange}
               />
               <button className="btn btn-outline-success" type="submit">
                 Search
@@ -88,7 +51,6 @@ const Header = () => {
           </div>
         </div>
       </nav>
-      
     </>
   );
 };
--- Basic Design/project-1/src/component/Main.jsx
+++ Basic Design/project-1/src/component/Main.jsx
@@ -3,6 +3,8 @@ import MultiActionAreaCard from "../sub-component/MultiActionAreaCard";
 
 const Main = () => {
   const [timeData, setTimeData] = useState(null);
+  const [searchQuery, setSearchQuery] = useState("");
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -10,13 +12,21 @@ const Main = () => {
         const response = await fetch("http://worldtimeapi.org/api/timezone");
         const data = await response.json();
         setTimeData(data);
+        setLoading(false);
       } catch (error) {
         console.error("Error fetching data:", error);
+        setLoading(false);
       }
     };
 
     fetchData();
   }, []);
+
+  const filteredTimeData = timeData
+    ? timeData.filter((item) =>
+        item.toLowerCase().includes(searchQuery.toLowerCase())
+      )
+    : [];
 
   return (
     <>
@@ -30,14 +40,24 @@ const Main = () => {
           flexWrap: "wrap",
         }}
       >
-        {timeData ? (
-          timeData.map((item, index) => (
+        <div style={{ marginBottom: "20px" }}>
+          <input
+            type="text"
+            placeholder="Search time zone"
+            value={searchQuery}
+            onChange={(e) => setSearchQuery(e.target.value)}
+          />
+        </div>
+        {loading ? (
+          <p>Loading...</p>
+        ) : filteredTimeData.length > 0 ? (
+          filteredTimeData.map((item, index) => (
             <div key={index} style={{ margin: "20px", maxWidth: "300px" }}>
               <MultiActionAreaCard data={item}></MultiActionAreaCard>
             </div>
           ))
         ) : (
-          <p>Loading...</p>
+          <p>No matching time zones found.</p>
         )}
       </div>
     </>
